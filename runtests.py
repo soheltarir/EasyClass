702acf918754fc0bea37e1134c76990107d38137
soheltarir <sohel.tarir@gmail.com>
2016-01-22 09:07:42 +0530

    Attempting to increase code coverage

--- runtests.py
+++ runtests.py
@@ -7,26 +7,59 @@ from easy_class import EasyClass, IntegerVariable, StringVariable, PositiveInteg
 class SomeClass(object):
     var = 1
 
-
-class TestClass(EasyClass):
-    a = IntegerVariable(max_value=10, min_value=-10)
-    b = StringVariable(null=False)
-    c = PositiveIntegerVariable()
-    d = FloatVariable()
-    e = BooleanVariable(default=False)
-    f = ClassVariable(cls=SomeClass)
-    g = DictVariable(default={"name": "Test"}, editable=False)
-    h = DateTimeVariable()
-    i = DateVariable()
-    j = TimeVariable()
+try:
+    class TestClass(EasyClass):
+        a = IntegerVariable(max_value=10, min_value=-10)
+        b = StringVariable(null=False)
+        c = PositiveIntegerVariable()
+        d = FloatVariable(max_value=0.5, min_value=0.1)
+        e = BooleanVariable(default=4)
+        f = ClassVariable(cls=SomeClass)
+        g = DictVariable(default={"name": "Test"}, editable=False)
+        h = DateTimeVariable()
+        i = DateVariable()
+        j = TimeVariable()
+except AttributeError:
+    class TestClass(EasyClass):
+        a = IntegerVariable(max_value=10, min_value=-10)
+        b = StringVariable(null=False)
+        c = PositiveIntegerVariable()
+        d = FloatVariable(max_value=0.5, min_value=0.1)
+        e = BooleanVariable(default=False)
+        f = ClassVariable(cls=SomeClass)
+        g = DictVariable(default={"name": "Test"}, editable=False)
+        h = DateTimeVariable()
+        i = DateVariable()
+        j = TimeVariable()
 
 
 def runtests():
-    obj = TestClass(b="Testing")
-    obj.a = 5
-    obj.c = 1
-    obj.d = 0.2
-    obj.f = SomeClass()
+    try:
+        obj = TestClass()
+    except ValueError:
+        obj = TestClass(b="Testing")
+    try:
+        obj.a = 11
+    except ValueError:
+        try:
+            obj.a = -11
+        except ValueError:
+            obj.a = 5
+    try:
+        obj.c = -1
+    except ValueError:
+        obj.c = 1
+    try:
+        obj.d = 0.6
+    except ValueError:
+        try:
+            obj.d = 0.05
+        except ValueError:
+            obj.d = 0.3
+    try:
+        obj.f = "Class"
+    except TypeError:
+        obj.f = SomeClass()
     obj.h = datetime(2016, 2, 1, hour=12)
     obj.i = '2016-02-01'
     obj.j = '12:00:00'
